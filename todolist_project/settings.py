db6a96ca309600d42da0d963cde279b3782f9575
HUAN <terikihuan@gmail.com>
2022-12-24 20:27:16 +0700

    change settings.py

--- todolist_project/settings.py
+++ todolist_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3--v0_r4ee-nkh3308u*g!8c*d0bo$_)^++mfttzyl)i6=ah1a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'huan-todolist.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
